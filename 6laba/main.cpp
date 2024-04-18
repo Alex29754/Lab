70cd40056b317dd891cb110912ed8df75811a3b9
Alex29754 <145469935+Alex29754@users.noreply.github.com>
2024-04-18 21:58:46 +0200

    +

---
+++ 6laba/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <SFML/Graphics.hpp>
+#include "source.hpp"
+#include "Game.hpp"
+
+int main()
+{
+    pa::Game game(1920, 1080, "Game");
+    if (!game.Setup(5)) {
+        return -1;
+    }
+
+    game.Life();
+
+    return 0;
+}
